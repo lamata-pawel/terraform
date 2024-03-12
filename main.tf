1f41650882a70843fe5c1553d2602e4b600dec18
Pawel Dubiel <pawel.dubiel@glovoapp.com>
2024-03-12 21:44:23 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,12 +16,6 @@ provider "digitalocean" {
   token = var.do_token
 }
 
-resource "digitalocean_project" "costa" {
-  name        = "Costa"
-  description = "Costa"
-  purpose     = "Web Application"
-  environment = "Production"
-}
 
 resource "digitalocean_ssh_key" "default" {
   name       = "Terraform"
@@ -31,6 +25,21 @@ resource "digitalocean_ssh_key" "default" {
 variable "ssh_public_key" {
   description = "The public SSH key for DigitalOcean droplet access"
   type        = string
+}
+
+resource "digitalocean_project" "costa" {
+  name        = "Costa"
+  description = "Costa"
+  purpose     = "Web Application"
+  environment = "Development"
+}
+
+resource "digitalocean_project_resources" "web_project" {
+  project = digitalocean_project.costa.id
+
+  resources = [
+    digitalocean_droplet.web.urn,
+  ]
 }
 
 resource "digitalocean_droplet" "web" {
@@ -60,7 +69,6 @@ resource "digitalocean_droplet" "web" {
               systemctl enable nginx
               EOF
 
-  # Specify the project ID
-  project = digitalocean_project.example.id
+
 }
 
