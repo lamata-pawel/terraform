4c1cc53efdf57f9127fd897d6466e0a5e7ef0cce
Pawel Dubiel <pawel.dubiel@glovoapp.com>
2024-03-12 23:05:24 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -42,33 +42,5 @@ resource "digitalocean_project_resources" "web_project" {
   ]
 }
 
-resource "digitalocean_droplet" "client1" {
-  name   = "app"
-  size   = "s-1vcpu-1gb"  # This is a basic droplet size. Adjust according to your needs.
-  image  = "ubuntu-20-04-x64"  # This is the slug for the Ubuntu 20.04 image. You can choose other images.
-  region = "nyc1"  # This is the New York City region. Choose the region closest to your users.
 
-  # SSH keys
-  ssh_keys = [digitalocean_ssh_key.default.fingerprint] # Ensure you have an SSH key added to your DigitalOcean account and reference its fingerprint here.
-
-  # Networking
-  private_networking = true
-  ipv6 = true
-
-  # Firewall
-  # You can associate a DigitalOcean Cloud Firewall with your droplet for enhanced security
-  # firewall_ids = [digitalocean_firewall.your_firewall.id]
-
-  # User data for initial setup
-  # Use this to provide a cloud-config script or other user data for configuring the droplet on first boot
-  user_data = <<-EOF
-              #!/bin/bash
-              apt-get update
-              apt-get install -y nginx
-              systemctl start nginx
-              systemctl enable nginx
-              EOF
-
-
-}
 
